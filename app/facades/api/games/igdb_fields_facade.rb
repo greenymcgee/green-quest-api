e43be907ac17e1d4be78039bf0d6e7098fd37e56
greenymcgee <greenhouston@gmail.com>
2024-11-16 11:51:08 -0500

    #58 Removes IGDB ID Properties from Games

--- app/facades/api/games/igdb_fields_facade.rb
+++ app/facades/api/games/igdb_fields_facade.rb
@@ -8,76 +8,20 @@ class Api::Games::IgdbFieldsFacade
 
   def populate_game_fields
     @@game.update(
-      age_rating_ids: age_rating_ids,
-      alternative_name_ids: alternative_name_ids,
-      artwork_ids: artwork_ids,
-      bundle_ids: bundle_ids,
       category_enum: category_enum,
       checksum: checksum,
-      collection_id: collection_id,
-      collection_ids: collection_ids,
-      cover_id: cover_id,
-      dlc_ids: dlc_ids,
-      expanded_game_ids: expanded_game_ids,
-      expansion_ids: expansion_ids,
-      external_game_ids: external_game_ids,
       first_release_date: first_release_date,
-      fork_ids: fork_ids,
-      franchise_id: franchise_id,
-      franchise_ids: franchise_ids,
-      game_engine_ids: game_engine_ids,
-      game_localization_ids: game_localization_ids,
-      game_mode_ids: game_mode_ids,
-      genre_ids: genre_ids,
       igdb_url: igdb_url,
-      involved_company_ids: involved_company_ids,
-      keyword_ids: keyword_ids,
-      language_support_ids: language_support_ids,
-      multiplayer_mode_ids: multiplayer_mode_ids,
       name: name,
-      parent_game_id: parent_game_id,
-      platform_ids: platform_ids,
-      player_perspective_ids: player_perspective_ids,
-      port_ids: port_ids,
-      release_date_ids: release_date_ids,
-      remake_ids: remake_ids,
-      remaster_ids: remaster_ids,
-      screenshot_ids: screenshot_ids,
-      similar_game_ids: similar_game_ids,
       slug: slug,
-      standalone_expansion_ids: standalone_expansion_ids,
       status: status,
       storyline: storyline,
       summary: summary,
-      tag_ids: tag_ids,
-      theme_ids: theme_ids,
-      version_parent_id: version_parent_id,
       version_title: version_title,
-      video_ids: video_ids,
-      website_ids: website_ids,
     )
   end
 
   private
-
-  def age_rating_ids
-    get_present_value @@game.age_rating_ids, @@igdb_game_data["age_ratings"]
-  end
-
-  def alternative_name_ids
-    get_present_value(
-      @@game.alternative_name_ids,
-      @@igdb_game_data["alternative_names"],
-    )
-  end
-
-  def artwork_ids
-    get_present_value(@@game.artwork_ids, @@igdb_game_data["artworks"])
-  end
-
-  def bundle_ids
-    get_present_value(@@game.bundle_ids, @@igdb_game_data["bundles"])
-  end
 
   def category_enum
     get_present_value(@@game.category_enum, @@igdb_game_data["category"])
@@ -85,40 +29,6 @@ class Api::Games::IgdbFieldsFacade
 
   def checksum
     get_present_value(@@game.checksum, @@igdb_game_data["checksum"])
-  end
-
-  def collection_id
-    get_present_value(@@game.collection_id, @@igdb_game_data["collection"])
-  end
-
-  def collection_ids
-    get_present_value(@@game.collection_ids, @@igdb_game_data["collections"])
-  end
-
-  def cover_id
-    get_present_value(@@game.cover_id, @@igdb_game_data["cover"])
-  end
-
-  def dlc_ids
-    get_present_value(@@game.dlc_ids, @@igdb_game_data["dlcs"])
-  end
-
-  def expanded_game_ids
-    get_present_value(
-      @@game.expanded_game_ids,
-      @@igdb_game_data["expanded_games"],
-    )
-  end
-
-  def expansion_ids
-    get_present_value(@@game.expansion_ids, @@igdb_game_data["expansions"])
-  end
-
-  def external_game_ids
-    get_present_value(
-      @@game.external_game_ids,
-      @@igdb_game_data["external_games"],
-    )
   end
 
   def first_release_date_unix_timestamp
@@ -132,125 +42,16 @@ class Api::Games::IgdbFieldsFacade
   def first_release_date
     get_present_value(@@game.first_release_date, first_release_date_to_datetime)
   end
-
-  def fork_ids
-    get_present_value(@@game.fork_ids, @@igdb_game_data["forks"])
-  end
-
-  def franchise_id
-    get_present_value(@@game.franchise_id, @@igdb_game_data["franchise"])
-  end
-
-  def franchise_ids
-    get_present_value(@@game.franchise_ids, @@igdb_game_data["franchises"])
-  end
-
-  def game_engine_ids
-    get_present_value(@@game.game_engine_ids, @@igdb_game_data["game_engines"])
-  end
-
-  def game_localization_ids
-    get_present_value(
-      @@game.game_localization_ids,
-      @@igdb_game_data["game_localizations"],
-    )
-  end
-
-  def game_mode_ids
-    get_present_value(@@game.game_mode_ids, @@igdb_game_data["game_modes"])
-  end
-
-  def genre_ids
-    get_present_value(@@game.genre_ids, @@igdb_game_data["genres"])
-  end
-
   def igdb_url
     get_present_value(@@game.igdb_url, @@igdb_game_data["url"])
-  end
-
-  def involved_company_ids
-    get_present_value(
-      @@game.involved_company_ids,
-      @@igdb_game_data["involved_companies"],
-    )
-  end
-
-  def keyword_ids
-    get_present_value(@@game.keyword_ids, @@igdb_game_data["keywords"])
-  end
-
-  def language_support_ids
-    get_present_value(
-      @@game.language_support_ids,
-      @@igdb_game_data["language_supports"],
-    )
-  end
-
-  def multiplayer_mode_ids
-    get_present_value(
-      @@game.multiplayer_mode_ids,
-      @@igdb_game_data["multiplayer_modes"],
-    )
   end
 
   def name
     get_present_value(@@game.name, @@igdb_game_data["name"])
   end
 
-  def parent_game_id
-    get_present_value(@@game.parent_game_id, @@igdb_game_data["parent_game"])
-  end
-
-  def platform_ids
-    get_present_value(@@game.platform_ids, @@igdb_game_data["platforms"])
-  end
-
-  def player_perspective_ids
-    get_present_value(
-      @@game.player_perspective_ids,
-      @@igdb_game_data["player_perspectives"],
-    )
-  end
-
-  def port_ids
-    get_present_value(@@game.port_ids, @@igdb_game_data["ports"])
-  end
-
-  def release_date_ids
-    get_present_value(
-      @@game.release_date_ids,
-      @@igdb_game_data["release_dates"],
-    )
-  end
-
-  def remake_ids
-    get_present_value(@@game.remake_ids, @@igdb_game_data["remakes"])
-  end
-
-  def remaster_ids
-    get_present_value(@@game.remaster_ids, @@igdb_game_data["remasters"])
-  end
-
-  def screenshot_ids
-    get_present_value(@@game.screenshot_ids, @@igdb_game_data["screenshots"])
-  end
-
-  def similar_game_ids
-    get_present_value(
-      @@game.similar_game_ids,
-      @@igdb_game_data["similar_games"],
-    )
-  end
-
   def slug
     get_present_value(@@game.slug, @@igdb_game_data["slug"])
-  end
-
-  def standalone_expansion_ids
-    get_present_value(
-      @@game.standalone_expansion_ids,
-      @@igdb_game_data["standalone_expansions"],
-    )
   end
 
   def status
@@ -265,30 +66,7 @@ class Api::Games::IgdbFieldsFacade
     get_present_value(@@game.summary, @@igdb_game_data["summary"])
   end
 
-  def tag_ids
-    get_present_value(@@game.tag_ids, @@igdb_game_data["tags"])
-  end
-
-  def theme_ids
-    get_present_value(@@game.theme_ids, @@igdb_game_data["themes"])
-  end
-
-  def version_parent_id
-    get_present_value(
-      @@game.version_parent_id,
-      @@igdb_game_data["version_parent"],
-    )
-  end
-
   def version_title
     get_present_value(@@game.version_title, @@igdb_game_data["version_title"])
-  end
-
-  def video_ids
-    get_present_value(@@game.video_ids, @@igdb_game_data["videos"])
-  end
-
-  def website_ids
-    get_present_value(@@game.website_ids, @@igdb_game_data["websites"])
   end
 end
--- test/facades/api/games/igdb_fields_facade_test.rb
+++ test/facades/api/games/igdb_fields_facade_test.rb
@@ -8,25 +8,6 @@ class Api::Games::IgdbFieldsFacadeTest < ActionDispatch::IntegrationTest
     facade.populate_game_fields
   end
 
-  test "should populate the age_rating_ids" do
-    assert_equal(@game.age_rating_ids, @igdb_game_data["age_ratings"])
-  end
-
-  test "should populate the alternative_name_ids" do
-    assert_equal(
-      @game.alternative_name_ids,
-      @igdb_game_data["alternative_names"],
-    )
-  end
-
-  test "should populate the artwork_ids" do
-    assert_equal(@game.artwork_ids, @igdb_game_data["artworks"])
-  end
-
-  test "should populate the bundle_ids" do
-    assert_equal(@game.bundle_ids, @igdb_game_data["bundles"])
-  end
-
   test "should populate the category_enum" do
     assert_equal(@game.category_enum, @igdb_game_data["category"])
   end
@@ -35,148 +16,18 @@ class Api::Games::IgdbFieldsFacadeTest < ActionDispatch::IntegrationTest
     assert_equal(@game.checksum, @igdb_game_data["checksum"])
   end
 
-  test "should populate the collection_id" do
-    assert_equal(@game.collection_id, @igdb_game_data["collection"])
-  end
-
-  test "should populate the collection_ids" do
-    assert_equal(@game.collection_ids, @igdb_game_data["collections"])
-  end
-
-  test "should populate the cover_id" do
-    assert_equal(@game.cover_id, @igdb_game_data["cover"])
-  end
-
-  test "should populate the dlc_ids" do
-    assert_equal(@game.dlc_ids, @igdb_game_data["dlcs"])
-  end
-
-  test "should populate the expanded_game_ids" do
-    assert_equal(@game.expanded_game_ids, @igdb_game_data["expanded_games"])
-  end
-
-  test "should populate the expansion_ids" do
-    assert_equal(@game.expansion_ids, @igdb_game_data["expansions"])
-  end
-
-  test "should populate the external_game_ids" do
-    assert_equal(@game.external_game_ids, @igdb_game_data["external_games"])
-  end
-
   test "should populate the first_release_date" do
     igdb_date = @igdb_game_data["first_release_date"]
     date = Time.at(igdb_date).utc.to_datetime
     assert_equal(@game.first_release_date, date)
   end
 
-  test "should populate the fork_ids" do
-    assert_equal(@game.fork_ids, @igdb_game_data["forks"])
-  end
-
-  test "should populate the franchise_id" do
-    assert_equal(@game.franchise_id, @igdb_game_data["franchise"])
-  end
-
-  test "should populate the franchise_ids" do
-    assert_equal(@game.franchise_ids, @igdb_game_data["franchises"])
-  end
-
-  test "should populate the game_engine_ids" do
-    assert_equal(@game.game_engine_ids, @igdb_game_data["game_engines"])
-  end
-
-  test "should populate the game_localization_ids" do
-    assert_equal(
-      @game.game_localization_ids,
-      @igdb_game_data["game_localizations"],
-    )
-  end
-
-  test "should populate the game_mode_ids" do
-    assert_equal(@game.game_mode_ids, @igdb_game_data["game_modes"])
-  end
-
-  test "should populate the genre_ids" do
-    assert_equal(@game.genre_ids, @igdb_game_data["genres"])
-  end
-
-  test "should populate the involved_company_ids" do
-    assert_equal(
-      @game.involved_company_ids,
-      @igdb_game_data["involved_companies"],
-    )
-  end
-
-  test "should populate the keyword_ids" do
-    assert_equal(@game.keyword_ids, @igdb_game_data["keywords"])
-  end
-
-  test "should populate the language_support_ids" do
-    assert_equal(
-      @game.language_support_ids,
-      @igdb_game_data["language_supports"],
-    )
-  end
-
-  test "should populate the multiplayer_mode_ids" do
-    assert_equal(
-      @game.multiplayer_mode_ids,
-      @igdb_game_data["multiplayer_modes"],
-    )
-  end
-
   test "should populate the name" do
     assert_equal(@game.name, @igdb_game_data["name"])
   end
 
-  test "should populate the parent_game_id" do
-    assert_equal(@game.parent_game_id, @igdb_game_data["parent_game"])
-  end
-
-  test "should populate the platform_ids" do
-    assert_equal(@game.platform_ids, @igdb_game_data["platforms"])
-  end
-
-  test "should populate the player_perspective_ids" do
-    assert_equal(
-      @game.player_perspective_ids,
-      @igdb_game_data["player_perspectives"],
-    )
-  end
-
-  test "should populate the port_ids" do
-    assert_equal(@game.port_ids, @igdb_game_data["ports"])
-  end
-
-  test "should populate the release_date_ids" do
-    assert_equal(@game.release_date_ids, @igdb_game_data["release_dates"])
-  end
-
-  test "should populate the remake_ids" do
-    assert_equal(@game.remake_ids, @igdb_game_data["remakes"])
-  end
-
-  test "should populate the remaster_ids" do
-    assert_equal(@game.remaster_ids, @igdb_game_data["remasters"])
-  end
-
-  test "should populate the screenshot_ids" do
-    assert_equal(@game.screenshot_ids, @igdb_game_data["screenshots"])
-  end
-
-  test "should populate the similar_game_ids" do
-    assert_equal(@game.similar_game_ids, @igdb_game_data["similar_games"])
-  end
-
   test "should populate the slug" do
     assert_equal(@game.slug, @igdb_game_data["slug"])
-  end
-
-  test "should populate the standalone_expansion_ids" do
-    assert_equal(
-      @game.standalone_expansion_ids,
-      @igdb_game_data["standalone_expansions"],
-    )
   end
 
   test "should populate the status" do
@@ -191,27 +42,7 @@ class Api::Games::IgdbFieldsFacadeTest < ActionDispatch::IntegrationTest
     assert_equal(@game.summary, @igdb_game_data["summary"])
   end
 
-  test "should populate the tag_ids" do
-    assert_equal(@game.tag_ids, @igdb_game_data["tags"])
-  end
-
-  test "should populate the theme_ids" do
-    assert_equal(@game.theme_ids, @igdb_game_data["themes"])
-  end
-
-  test "should populate the version_parent_id" do
-    assert_equal(@game.version_parent_id, @igdb_game_data["version_parent"])
-  end
-
   test "should populate the version_title" do
     assert_equal(@game.version_title, @igdb_game_data["version_title"])
-  end
-
-  test "should populate the video_ids" do
-    assert_equal(@game.video_ids, @igdb_game_data["videos"])
-  end
-
-  test "should populate the website_ids" do
-    assert_equal(@game.website_ids, @igdb_game_data["websites"])
   end
 end

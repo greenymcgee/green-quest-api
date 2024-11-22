5ad885474ad6f955fac2dd24a557c6614c0dc57f
greenymcgee <greenhouston@gmail.com>
2024-11-22 07:15:18 -0700

    #117 Add Fields Facade for Genres

---
+++ app/facades/api/genres/igdb_fields_facade.rb
@@ -0,0 +1,35 @@
+class Api::Genres::IgdbFieldsFacade
+  include IgdbFieldsHelper
+
+  def initialize(genre, igdb_data)
+    @@genre = genre
+    @@igdb_data = igdb_data
+  end
+
+  def populate_genre_fields
+    @@genre.update(
+      checksum: checksum,
+      igdb_url: igdb_url,
+      name: name,
+      slug: slug,
+    )
+  end
+
+  private
+
+  def checksum
+    get_present_value(@@genre.checksum, @@igdb_data["checksum"])
+  end
+
+  def igdb_url
+    get_present_value(@@genre.igdb_url, @@igdb_data["url"])
+  end
+
+  def name
+    get_present_value(@@genre.name, @@igdb_data["name"])
+  end
+
+  def slug
+    get_present_value(@@genre.slug, @@igdb_data["slug"])
+  end
+end
---
+++ test/facades/api/genres/igdb_fields_facade_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+class Api::Genres::IgdbFieldsFacadeTest < ActionDispatch::IntegrationTest
+  setup do
+    @genre = Genre.new(igdb_id: 1026)
+    @igdb_data, = JSON.parse(json_mocks("igdb/genres/9.json"))
+    facade = Api::Genres::IgdbFieldsFacade.new(@genre, @igdb_data)
+    facade.populate_genre_fields
+  end
+
+  test "should populate the checksum" do
+    assert_equal(@genre.checksum, @igdb_data["checksum"])
+  end
+
+  test "should populate the igdb_url" do
+    assert_equal(@genre.igdb_url, @igdb_data["url"])
+  end
+
+  test "should populate the name" do
+    assert_equal(@genre.name, @igdb_data["name"])
+  end
+
+  test "should populate the slug" do
+    assert_equal(@genre.slug, @igdb_data["slug"])
+  end
+end

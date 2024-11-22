cce060523380b24edaf4a138034f76af65e20e4d
greenymcgee <greenhouston@gmail.com>
2024-11-22 07:45:17 -0700

    #116 Adds Igdb Fields Facade for Companies

---
+++ app/facades/api/companies/igdb_fields_facade.rb
@@ -0,0 +1,102 @@
+class Api::Companies::IgdbFieldsFacade
+  include IgdbFieldsHelper
+
+  def initialize(company, igdb_data)
+    @@company = company
+    @@igdb_data = igdb_data
+  end
+
+  def populate_company_fields
+    @@company.update(
+      change_date: change_date,
+      change_date_category_enum: change_date_category_enum,
+      changed_company_id: changed_company_id,
+      checksum: checksum,
+      country_code: country_code,
+      developed_game_ids: developed_game_ids,
+      description: description,
+      igdb_url: igdb_url,
+      name: name,
+      parent_id: parent_id,
+      published_game_ids: published_game_ids,
+      slug: slug,
+      start_date: start_date,
+    )
+  end
+
+  private
+
+  def change_date_unix_timestamp
+    @@igdb_data["change_date"]
+  end
+
+  def change_date_to_datetime
+    Time.at(change_date_unix_timestamp).utc.to_datetime
+  end
+
+  def change_date
+    get_present_value(@@company.change_date, change_date_to_datetime)
+  end
+
+  def change_date_category_enum
+    get_present_value(
+      @@company.change_date_category_enum,
+      @@igdb_data["change_date_category"],
+    )
+  end
+
+  def changed_company_id
+    get_present_value(
+      @@company.changed_company_id,
+      @@igdb_data["changed_company_id"],
+    )
+  end
+
+  def checksum
+    get_present_value(@@company.checksum, @@igdb_data["checksum"])
+  end
+
+  def country_code
+    get_present_value(@@company.country_code, @@igdb_data["country"])
+  end
+
+  def description
+    get_present_value(@@company.description, @@igdb_data["description"])
+  end
+
+  def developed_game_ids
+    get_present_value(@@company.developed_game_ids, @@igdb_data["developed"])
+  end
+
+  def igdb_url
+    get_present_value(@@company.igdb_url, @@igdb_data["url"])
+  end
+
+  def name
+    get_present_value(@@company.name, @@igdb_data["name"])
+  end
+
+  def parent_id
+    get_present_value(@@company.parent_id, @@igdb_data["parent"])
+  end
+
+  def published_game_ids
+    get_present_value(@@company.published_game_ids, @@igdb_data["published"])
+  end
+
+  def slug
+    get_present_value(@@company.slug, @@igdb_data["slug"])
+  end
+
+  def start_date_unix_timestamp
+    @@igdb_data["start_date"]
+  end
+
+  def start_date_to_datetime
+    Time.at(start_date_unix_timestamp).utc.to_datetime
+  end
+
+  def start_date
+    get_present_value(@@company.start_date, start_date_to_datetime)
+  end
+end
---
+++ test/facades/api/companies/igdb_fields_facade_test.rb
@@ -0,0 +1,65 @@
+require "test_helper"
+
+class Api::Companies::IgdbFieldsFacadeTest < ActionDispatch::IntegrationTest
+  setup do
+    @company = Company.new(igdb_id: 1026)
+    @igdb_data, = JSON.parse(json_mocks("igdb/companies/421.json"))
+    facade = Api::Companies::IgdbFieldsFacade.new(@company, @igdb_data)
+    facade.populate_company_fields
+  end
+
+  test "should populate the change_date" do
+    igdb_date = @igdb_data["change_date"]
+    date = Time.at(igdb_date).utc.to_datetime
+    assert_equal(@company.change_date, date)
+  end
+
+  test "should populate the change_date_category_enum" do
+    assert_equal(
+      @company.change_date_category_enum,
+      @igdb_data["change_date_category"],
+    )
+  end
+
+  test "should populate the changed_company_id" do
+    assert_equal(@company.changed_company_id, @igdb_data["changed_company_id"])
+  end
+
+  test "should populate the checksum" do
+    assert_equal(@company.checksum, @igdb_data["checksum"])
+  end
+
+  test "should populate the country_code" do
+    assert_equal(@company.country_code, @igdb_data["country"])
+  end
+
+  test "should populate the description" do
+    assert_equal(@company.description, @igdb_data["description"])
+  end
+
+  test "should populate the developed_game_ids" do
+    assert_equal(@company.developed_game_ids, @igdb_data["developed"])
+  end
+
+  test "should populate the igdb_url" do
+    assert_equal(@company.igdb_url, @igdb_data["url"])
+  end
+
+  test "should populate the name" do
+    assert_equal(@company.name, @igdb_data["name"])
+  end
+
+  test "should populate the parent_id" do
+    assert_equal(@company.parent_id, @igdb_data["parent"])
+  end
+
+  test "should populate the slug" do
+    assert_equal(@company.slug, @igdb_data["slug"])
+  end
+
+  test "should populate the start_date" do
+    igdb_date = @igdb_data["start_date"]
+    date = Time.at(igdb_date).utc.to_datetime
+    assert_equal(@company.start_date, date)
+  end
+end

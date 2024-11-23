d7750c67c15d00a4b43422d4eb8e579b9535c118
greenymcgee <greenhouston@gmail.com>
2024-11-23 08:47:26 -0700

    #104 Adds Involved Companies to Games

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,32 @@ class Game < ApplicationRecord
   has_many :involved_companies
 
   validates :igdb_id, presence: true
+
+  def developers
+    developer_involved_companies = involved_companies.where(is_developer: true)
+    developer_involved_companies.map do |involved_company|
+      involved_company.company
+    end
+  end
+
+  def porters
+    porter_involved_companies = involved_companies.where(is_porter: true)
+    porter_involved_companies.map do |involved_company|
+      involved_company.company
+    end
+  end
+
+  def publishers
+    publisher_involved_companies = involved_companies.where(is_publisher: true)
+    publisher_involved_companies.map do |involved_company|
+      involved_company.company
+    end
+  end
+
+  def supporters
+    supporter_involved_companies = involved_companies.where(is_supporter: true)
+    supporter_involved_companies.map do |involved_company|
+      involved_company.company
+    end
+  end
 end
--- app/views/api/games/_game.json.jbuilder
+++ app/views/api/games/_game.json.jbuilder
@@ -1,3 +1,4 @@
+json.errors game.errors if game.errors.present?
 json.extract!(
   game,
   :id,
@@ -8,6 +9,9 @@ json.extract!(
   :created_at,
   :updated_at,
 )
+json.developers game.developers do |company|
+  json.partial! "api/companies/company", company: company
+end
 json.genres game.genres do |genre|
   json.extract! genre, :id, :igdb_id, :name, :slug, :created_at, :updated_at
 end
@@ -26,4 +30,12 @@ json.platforms game.platforms do |platform|
     :updated_at,
   )
 end
-json.errors game.errors if game.errors.present?
+json.porters game.porters do |company|
+  json.partial! "api/companies/company", company: company
+end
+json.publishers game.publishers do |company|
+  json.partial! "api/companies/company", company: company
+end
+json.supporters game.supporters do |company|
+  json.partial! "api/companies/company", company: company
+end

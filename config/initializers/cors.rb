12271b15df87ea21d89b8190b58b8ce9d8a9237d
greenymcgee <greenhouston@gmail.com>
2024-12-16 18:37:59 -0700

    Updates CORS Config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins([Rails.configuration.frontend_host])
+    origins([ENV["FRONTEND_HOST"]])
 
     resource(
       "*",
